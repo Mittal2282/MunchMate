8b13b062eac811a54fce03149343f2f4be6013bd
Mittal2282 <108392982+Mittal2282@users.noreply.github.com>
2024-05-23 12:55:19 +0530

    Basics of React

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
-/* <div id="parent">
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-    <div id="child1">
-        <h1>I am h1 tag from child 1</h1>
-        <h2>I am h2 tag from child 1</h2>
+const Jing = () => <h1 id="heading">Hello World from JSX !</h1>;
+// use angular brackets if code lies in different lines
+
+// react functional component
+const FunctionalHeading = () => {
+  return (
+    <div>
+      {Jing()}
+      <Jing />
+      <Jing></Jing>
+      {/* // these 3 ways are same thing */}
+      <h1 className="heading">Hello From React Functional Components</h1>
     </div>
+  );
+};
 
-    <div id="child2">
-        <h1>I am h1 tag from child 2</h1>
-        <h2>I am h2 tag from child 2</h2>
-    </div>
-
-</div> */
-
-const heading2=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"I am a h1 tag from child 1"),
-        React.createElement("h2",{},"I am a h2 tag from child 1"),
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I am a h1 tag from child 2"),
-        React.createElement("h2",{},"I am a h2 tag from child 2"),
-    ]),
-]);
-
-const heading=React.createElement("h1",{id:"heading"},"Hello World From React!");
-// heading is an object and not a heading tag yet
-// second argument for createElement is attributes to the tag
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading2);
-// render method is responsible for converting that object into heading tag and show on the browser
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalHeading />);
+// this is how we can render a functional component
+// because that is how the babel will understand it
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Learning React Basics
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm-> written in C++
+- Caching-> Faster builds
+- Image Optimisation
+- Minification-> in case of production builds
+- Bundling
+- Compression of files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling-> ie supports older browsers as well
+- Diagnostics
+- Error Handling
+- Https
+- Tree Shaking-> removing the unused code
+- Different Dev and production bundles
+
+
